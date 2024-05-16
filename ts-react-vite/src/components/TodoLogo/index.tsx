3352dbe03f72c752547c83ab5a19c70c706dccf4
JaydenLee1116 <vv55adss@gmail.com>
2024-05-16 23:12:22 +0900

    TodoLogo 컴포넌트 추가

---
+++ ts-react-vite/src/components/TodoLogo/index.tsx
@@ -0,0 +1,9 @@
+function TodoLogo() {
+  return (
+    <header className="w-full text-center text-[#b83f45] text-8xl leading-[80px] m-0 font-light">
+      <h1>todos</h1>
+    </header>
+  );
+}
+
+export default TodoLogo;
---
+++ ts-react-vite/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import TodoLogo from "../../components/TodoLogo";
+
+function Home () {
+  return (
+    <div>
+      <TodoLogo />
+    </div>
+  )
+}
+
+export default Home;
