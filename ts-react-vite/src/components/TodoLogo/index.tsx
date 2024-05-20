5bbd6925e0c0d7afcdafa1e9d47c307966d75a00
JaydenLee1116 <vv55adss@gmail.com>
2024-05-20 23:09:34 +0900

    TodoLogo 폰트 크기 변경

--- ts-react-vite/src/components/TodoLogo/index.tsx
+++ ts-react-vite/src/components/TodoLogo/index.tsx
@@ -1,6 +1,6 @@
 function TodoLogo() {
   return (
-    <header className="w-full text-center text-[#b83f45] text-8xl leading-[80px] m-0 font-light">
+    <header className="w-full text-center text-[#b83f45] text-8xl leading-[80px] m-0 font-thin">
       <h1>todos</h1>
     </header>
   );
