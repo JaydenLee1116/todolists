5761b95e3d46113294d5730fa41420a1e299c2e5
JaydenLee1116 <vv55adss@gmail.com>
2024-05-23 23:02:08 +0900

    기본 투두리스트 완성

--- ts-react-vite/src/components/TodoLogo/index.tsx
+++
@@ -1,9 +0,0 @@
-function TodoLogo() {
-  return (
-    <header className="w-full text-center text-[#b83f45] text-8xl leading-[80px] m-0 font-thin">
-      <h1>todos</h1>
-    </header>
-  );
-}
-
-export default TodoLogo;
